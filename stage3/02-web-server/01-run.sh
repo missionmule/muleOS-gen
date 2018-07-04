51051a25b0fb891065a9baf89d049fab6948f479
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-07-04 14:19:23 -0700

    Added basic web server structure

---
+++ stage3/02-web-server/01-run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+# Make directories for sensor data and logs
+on_chroot << EOF
+
+mkdir -p /srv/logs
+mkdir -p /srv/data
+
+EOF
+
+# Download web server files into /var/www/html directory (where Apache2 serves from)
+on_chroot << EOF
+
+rm -rf /var/www/html
+mkdir -p /var/www/html
+
+cd /var/www/html && git clone -v https://github.com/missionmule/data-mule-server.git .
+
+EOF
+
+# Enable Apache2 access to /srv/ directory where sensor data and logs are kept
+echo "Installing apache2.conf..."
+
+on_chroot << EOF
+
+rm -rf /etc/apache2/apache2.conf
+
+EOF
+
+install -m 644 -v files/apache2.conf  "${ROOTFS_DIR}/etc/apache2"
