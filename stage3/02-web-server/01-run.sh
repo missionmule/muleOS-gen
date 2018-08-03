8c3c0c43032e190bf3d5cb310188ae8c92a3253d
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-08-03 14:17:26 -0700

    Added web server

---
+++ stage3/02-web-server/01-run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+on_chroot << EOF
+
+rm -rf /var/www/html
+mkdir -p /var/www/html
+
+cd /var/www/html && git clone -v https://github.com/missionmule/data-mule-server.git .
+
+mkdir -p /srv/flight-data/field
+mkdir -p /srv/flight-data/logs
+
+rm -rf /etc/apache2/apache2.conf
+
+EOF
+
+install -m 755 files/apache2.conf   "${ROOTFS_DIR}/etc/apache2/apache2.conf"
+
+on_chroot << EOF
+
+chown pi:pi /srv/
+chmod 755 /srv/
+
+EOF
