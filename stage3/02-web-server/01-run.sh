ecac78f19bc5b85a91eeb455302a7a9d484efe7f
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-09-17 17:19:14 -0700

    Removed old config install

--- stage3/02-web-server/01-run.sh
+++ stage3/02-web-server/01-run.sh
@@ -17,14 +17,3 @@ mkdir -p /var/www/html
 cd /var/www/html && git clone -v https://github.com/missionmule/data-mule-server.git .
 
 EOF
-#
-# # Enable Apache2 access to /srv/ directory where sensor data and logs are kept
-# echo "Installing apache2.conf..."
-#
-# on_chroot << EOF
-#
-# rm -rf /etc/apache2/apache2.conf
-#
-# EOF
-#
-# install -m 644 -v files/apache2.conf  "${ROOTFS_DIR}/etc/apache2"
