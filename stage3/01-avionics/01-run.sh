c8c475fc96a6a64a32cf4f5c583a0b7301a2392f
Zane Mountcastle <zane.mountcastle@gmail.com>
2019-03-04 11:25:16 -0800

    Another shot in the dark here

--- stage3/01-avionics/01-run.sh
+++ stage3/01-avionics/01-run.sh
@@ -12,13 +12,31 @@ cd /opt/mission-mule && git clone -v https://github.com/missionmule/firefly-mule
 
 cd /opt/mission-mule/firefly-mule && pip3 install -r /opt/mission-mule/firefly-mule/requirements.txt
 
+cd /opt/mission-mule && git clone -v https://github.com/missionmule/data-mule-server.git
+
+cd /opt/mission-mule/data-mule-server && yarn setup
+
+cd /opt/mission-mule/data-mule-server/client && yarn build
+
+rm -rf /etc/nginx/sites-available/default
+
+mkdir -p /srv/
+chown pi:pi -R /srv/
+chmod 755 /srv/
+
 EOF
 
 install -m 644 files/mission-mule-avionics.service   "${ROOTFS_DIR}/lib/systemd/system/"
+install -m 644 files/mission-mule-client.service   "${ROOTFS_DIR}/lib/systemd/system/"
+install -m 644 files/mission-mule-server.service   "${ROOTFS_DIR}/lib/systemd/system/"
+install -m 644 files/default   "${ROOTFS_DIR}/etc/nginx/sites-available/"
 
 on_chroot << EOF
 
 systemctl enable mission-mule-avionics
 systemctl disable hciuart
+systemctl enable mission-mule-client
+systemctl enable mission-mule-server
+systemctl enable nginx
 
 EOF
