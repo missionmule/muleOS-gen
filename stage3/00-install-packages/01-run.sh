f9a477aa9133f569df140475a3ef8fb04f63007e
Zane Mountcastle <zane.mountcastle@gmail.com>
2019-03-05 13:59:30 -0800

    Minor node/yarn/npm build tweaks

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -10,6 +10,12 @@ pip3 install geopy
 
 EOF
 
-curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+
+apt-get remove cmdtest && apt-get update && apt-get install yarn nodejs -y
+
+yarn add global react-scripts
+yarn add global serve
